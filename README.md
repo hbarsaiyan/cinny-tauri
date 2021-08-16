4ec2067bc0bb9b7dde3eaddbdd6f0804c6bd6a05
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2021-08-16 17:22:57 +0530

    init tauri

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">Cinny Tauri</h1>
+
+# FAQ
+
+### What is Cinny Tauri?
+It's an unofficial wrapper for [Cinny](https://cinny.in) that uses the lightweight library [Tauri](https://tauri.studio).
+
+# Contributing
+
+Contributions are absolutely appreciated!
+Feel free to tackle open issues, add something neat, etc. whatever you feel like. :)
+
+## Tauri Dependencies
+
+First, you will need the dependencies needed for Tauri.
+You can see the process depending on your operating system [here](https://tauri.studio/en/docs/getting-started/intro)!
+
+### Clone the repository
+```ps
+git clone https://github.com/DiscordTauri/discord-tauri.git
+```
+### Install the project dependencies
+```ps
+npm install
+```
+### Check to Make Sure Everything Is Set up Properly:
+```ps
+npm run tauri info
+```
+At this point, you can edit the code!
+### Run cinny-tauri
+```ps
+npm run tauri dev
+```
+### Build cinny-tauri
+The binary file will be in /src-tauri/target/release/
+```ps
+npm run tauri build
+```
+
+## Special Thanks to:
+
+@ajbura for creating [Cinny](https://github.com/ajbura/cinny)
+@RealDrPuc for creating [discord-tauri](https://github.com/DiscordTauri/discord-tauri) which has served as a template for this project.
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,66 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+// Tauri imports
+use tauri::{CustomMenuItem, Manager, SystemTrayEvent, SystemTrayMenu, SystemTray};
+
+fn main() {
+	
+  // We can't make a separate file for a Desktop Tray yet, so we make it here
+  // Create an item of the context menu called `quit` with the string `Quit Cinny`
+  let quit_item = CustomMenuItem::new("quit".to_string(), "Quit Cinny");
+  // Create a new context menu with the item above
+  let context_menu = SystemTrayMenu::new().add_item(quit_item);
+  // Create a new system tray with the context menu above
+  let desktop_tray = SystemTray::new().with_menu(context_menu);
+  
+  tauri::Builder::default()
+  
+	// Register the commands
+    .invoke_handler(tauri::generate_handler![])
+    // Register the desktop tray
+    .system_tray(desktop_tray)
+    // Add events for the desktop tray
+    .on_system_tray_event(|app, event| match event {
+      // If the event is a left click into the icon,
+      // verify that the window is closed and re-open it
+      // while setting it up again (Tauri doesn't save
+      // the status of hidden windows).
+	  SystemTrayEvent::LeftClick {
+        position: _,
+        size: _,
+        ..
+      } => {
+        let window = app.get_window("main").unwrap();
+        // If the window is closed
+        if !window.is_visible().unwrap() {
+          // Re-open the window
+          window.show().unwrap();
+          // Maximize it
+          window.maximize().unwrap();
+        }
+        // Even if the window isn't closed, it could be minimized in the taskbar; set it as focused
+        else {
+          window.set_focus().unwrap();
+        }
+      }
+      // If the event is a click to an item
+      SystemTrayEvent::MenuItemClick {id,..} => {
+        // We compare the name of the item to...
+        match id.as_str() {
+          // `quit` is the item with the string `Quit Cinny`
+          // If the user clicks quit, close cinny-tauri
+          "quit" => {
+            // Exit the process with an error code 0
+            std::process::exit(0);
+          }
+          _ => {}
+        }
+      }
+      _ => {}
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running cinny-tauri");
+}
